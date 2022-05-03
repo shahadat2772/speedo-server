4e196cfd7a5511f2da4cade8728ab61f97609f5f
shahadat2772 <shahadat2772@gmailcom>
2022-05-03 23:44:10 +0600

    secured the firebase config using env variable

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ app.use(express.json());
 app.use(cors());
 
 // Connecting DB
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.d7c9x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
